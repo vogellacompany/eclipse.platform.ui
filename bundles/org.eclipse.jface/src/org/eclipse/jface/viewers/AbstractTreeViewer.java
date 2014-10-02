0a2bbb54fb1204c38ce8745161119b31059cbfd8
Simon Scholz <simon.scholz@vogella.com>
2014-10-03 00:45:24 +0200

    Bug 441244 - Remove unnecessary (non-Javadoc) statements
    Change-Id: Ib6cdbf74689eccf2bac308faf5375459d412a3c9
    Signed-off-by: Simon Scholz <simon.scholz@vogella.com>

--- bundles/org.eclipse.jface/src/org/eclipse/jface/viewers/AbstractTreeViewer.java
+++ bundles/org.eclipse.jface/src/org/eclipse/jface/viewers/AbstractTreeViewer.java
@@ -1480,9 +1480,7 @@ public abstract class AbstractTreeViewer<E,I> extends ColumnViewer<E,I> {
 
 	/*
 	 * Overridden in AbstractTreeViewer to fix bug 108102 (code copied from
-	 * StructuredViewer to avoid introducing new API) (non-Javadoc)
-	 *
-	 * @see org.eclipse.jface.viewers.StructuredViewer#handleDoubleSelect(org.eclipse.swt.events.SelectionEvent)
+	 * StructuredViewer to avoid introducing new API)
 	 */
 	@Override
 	protected void handleDoubleSelect(SelectionEvent event) {
